9649f24dab11d35b53fad6215a8e897361fd266f
Kukks <evilkukka@gmail.com>
2024-11-29 09:46:36 +0100

    bump net 8, bump lib, add channel request actions

--- LNURL/LNURLChannelRequest.cs
+++ LNURL/LNURLChannelRequest.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using BTCPayServer.Lightning;
 using LNURL.JsonConverters;
+using NBitcoin;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace LNURL;
 
@@ -21,4 +26,35 @@ public class LNURLChannelRequest
     [JsonProperty("k1")] public string K1 { get; set; }
 
     [JsonProperty("tag")] public string Tag { get; set; }
+
+
+    public async Task SendRequest(PubKey ourId, bool privateChannel, HttpClient httpClient,
+        CancellationToken cancellationToken = default)
+    {
+        var url = Callback;
+        var uriBuilder = new UriBuilder(url);
+        LNURL.AppendPayloadToQuery(uriBuilder, "k1", K1);
+        LNURL.AppendPayloadToQuery(uriBuilder, "remoteid", ourId.ToString());
+        LNURL.AppendPayloadToQuery(uriBuilder, "private",privateChannel? "1":"0");
+    
+        url = new Uri(uriBuilder.ToString());
+        var response = await httpClient.GetAsync(url, cancellationToken);
+        var json = JObject.Parse(await response.Content.ReadAsStringAsync(cancellationToken));
+        if (LNUrlStatusResponse.IsErrorResponse(json, out var error)) throw new LNUrlException(error.Reason);
+
+    }
+
+    public async Task CancelRequest(PubKey ourId, HttpClient httpClient, CancellationToken cancellationToken = default)
+    {
+        var url = Callback;
+        var uriBuilder = new UriBuilder(url);
+        LNURL.AppendPayloadToQuery(uriBuilder, "k1", K1);
+        LNURL.AppendPayloadToQuery(uriBuilder, "remoteid", ourId.ToString());
+        LNURL.AppendPayloadToQuery(uriBuilder, "cancel", "1");
+        
+        url = new Uri(uriBuilder.ToString());
+        var response = await httpClient.GetAsync(url, cancellationToken);
+        var json = JObject.Parse(await response.Content.ReadAsStringAsync(cancellationToken));
+        if (LNUrlStatusResponse.IsErrorResponse(json, out var error)) throw new LNUrlException(error.Reason);
+    }
 }
